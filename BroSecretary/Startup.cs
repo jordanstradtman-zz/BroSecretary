36cd10e301b518289aa13d7e6f3cbef5349b2c2c
Jordy Stradtman <jordy.stradtman@gmail.com>
2018-04-12 05:51:39 -0500

    Initial Commit

---
+++ BroSecretary/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BroSecretary.Startup))]
+namespace BroSecretary
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
